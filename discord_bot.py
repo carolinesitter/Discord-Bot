54a8b5be961db55d37b71685af751d49fafc58f5
Caroline <carolinesitter24@gmail.com>
2022-05-31 20:10:23 -0500

    Set Up Discord Bot in discord_bot.py

---
+++ discord_bot.py
@@ -0,0 +1,24 @@
+import sys
+from random import choice
+import os
+import discord
+
+# Discord Bot Set Up
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.environ['DISCORD_TOKEN'])
