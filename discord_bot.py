08725327a24fbce06ced8cff21c2c245c65830ce
Caroline <carolinesitter24@gmail.com>
2022-05-31 20:39:36 -0500

    Added Wisdom to wisdom.txt

--- discord_bot.py
+++ discord_bot.py
@@ -19,9 +19,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # If the user says hello, say hello back
     if message.content.startswith('$hello'):
         await message.channel.send('Hello there!')
     
+    # If the user asks for wisdom, generate a random piece of wisdom
     elif message.content.startswith('$wisdom?'):
         await message.channel.send('you must complete your training before receiving this wisdom...check back later')
 
